423ca8b073a9ebb8982cc949656170ed0e378277
ranelpadon <ranel.padon@gmail.com>
2015-05-21 19:10:27 +0800

    Add the Panoramio template file

---
+++ summercamp_panoramio.tpl.php
@@ -0,0 +1,4 @@
+<!-- Render the photo and its title using the values passed to the
+  'summercamp_panoramio_template_file' theme definition. -->
+<p><?php print $title; ?></p>
+<img src=<?php print $src; ?> />
