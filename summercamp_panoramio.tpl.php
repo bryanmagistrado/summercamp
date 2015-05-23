7a165360f90946726e5a30e4e1a9ef0c8c4a2bde
ranelpadon <ranel.padon@gmail.com>
2015-05-23 18:08:28 +0800

    Use larger variant of the Panoramio photo

--- summercamp_panoramio.tpl.php
+++ summercamp_panoramio.tpl.php
@@ -1,4 +1,4 @@
 <!-- Render the photo and its title using the values passed to the
   'summercamp_panoramio_template_file' theme definition. -->
 <p><?php print $title; ?></p>
-<img src=<?php print $src; ?> />
+<img src=<?php print $src; ?> width='168px' />
